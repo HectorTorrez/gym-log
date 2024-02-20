49b55fd712a98c0cea2ebfe1041581fec28b28e6
Hector <torrezhectorb@gmail.com>
2024-02-20 14:17:38 -0600

    style: add new button to create more exercises

--- src/components/exerciseList.tsx
+++ src/components/exerciseList.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import {cn} from "@/lib/utils";
 
 import {Input} from "./ui/input";
+import {Dialog, DialogContent, DialogTrigger, DialogFooter} from "./ui/dialog";
+import {Button} from "./ui/button";
 import SelectCategory from "./SelectCategory";
+import FormAddExercise from "./formAddExercise";
 
 export default function ExerciseList({className, children}: React.HTMLAttributes<HTMLElement>) {
   return (
@@ -9,8 +14,16 @@ export default function ExerciseList({className, children}: React.HTMLAttributes
       <section>
         <Input placeholder="Search" />
       </section>
-      <section className="mt-3">
+      <section className="mt-3 flex justify-between">
         <SelectCategory />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>New</Button>
+          </DialogTrigger>
+          <DialogContent className="py-16">
+            <FormAddExercise />
+          </DialogContent>
+        </Dialog>
       </section>
       <section className="mt-10">{children}</section>
     </section>
