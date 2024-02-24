e705d0c06958bd35ac7476366e2ab8ca7cf054c0
Hector <torrezhectorb@gmail.com>
2024-02-24 15:01:17 -0600

    refactor: add a new function to get the token form clerk

--- src/db/api/server.ts
+++ src/db/api/server.ts
@@ -2,9 +2,17 @@ import type {Database} from "../types";
 
 import {createClient} from "@supabase/supabase-js";
 
-const supabase = createClient<Database>(
+export const supabaseClient = async (supabaseAccessToken: string) => {
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+    {global: {headers: {Authorization: `Bearer ${supabaseAccessToken}`}}},
+  );
+
+  return supabase;
+};
+
+export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_KEY!,
 );
-
-export default supabase;
