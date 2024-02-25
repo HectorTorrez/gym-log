d464f78b03ab190f4e606bfbceec957e96e41cc3
Hector <torrezhectorb@gmail.com>
2024-02-25 14:08:18 -0600

    style: restyle

--- src/api/getExercises.tsx
+++ src/api/getExercises.tsx
@@ -1,7 +1,12 @@
 import {supabase} from "@/db/api/server";
 
+export const revalidate = 0;
+
 export async function getExercises() {
-  const {data} = await supabase.from("exercise_list").select();
+  const {data} = await supabase
+    .from("exercise_list")
+    .select()
+    .order("created_at", {ascending: false});
 
   return data ?? [];
 }
