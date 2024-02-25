ee19fd34de4c4b060e3d2c01f0edcb6770888236
Hector <torrezhectorb@gmail.com>
2024-02-25 17:08:56 -0600

    refactor: add /exercises to ignoreroutes

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import {authMiddleware} from "@clerk/nextjs";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default authMiddleware({
   publicRoutes: ["/"],
+  ignoredRoutes: ["/exercises"],
 });
 
 export const config = {
