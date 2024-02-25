9c4fdd3beee36f4feafdd620809a1d9096176d3a
Hector <torrezhectorb@gmail.com>
2024-02-25 14:09:18 -0600

    refactor: gen type of my database

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,214 +1,232 @@
-export type Json = string | number | boolean | null | {[key: string]: Json | undefined} | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       exercise: {
         Row: {
-          created_at: string;
-          id: string;
-          name: string;
-          set: string;
-          template_id: string;
-          weight: string;
-        };
+          created_at: string
+          id: string
+          name: string
+          set: string
+          template_id: string
+          weight: string
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          name: string;
-          set: string;
-          template_id?: string;
-          weight: string;
-        };
+          created_at?: string
+          id?: string
+          name: string
+          set: string
+          template_id?: string
+          weight: string
+        }
         Update: {
-          created_at?: string;
-          id?: string;
-          name?: string;
-          set?: string;
-          template_id?: string;
-          weight?: string;
-        };
+          created_at?: string
+          id?: string
+          name?: string
+          set?: string
+          template_id?: string
+          weight?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "public_exercise_template_id_fkey";
-            columns: ["template_id"];
-            isOneToOne: false;
-            referencedRelation: "template";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
+            foreignKeyName: "public_exercise_template_id_fkey"
+            columns: ["template_id"]
+            isOneToOne: false
+            referencedRelation: "template"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       exercise_list: {
         Row: {
-          category: string;
-          created_at: string;
-          id: string;
-          name: string;
-          type: string;
-        };
+          category: string
+          created_at: string
+          id: string
+          name: string
+          role: string | null
+          type: string
+          user_id: string | null
+        }
         Insert: {
-          category: string;
-          created_at?: string;
-          id?: string;
-          name: string;
-          type: string;
-        };
+          category: string
+          created_at?: string
+          id?: string
+          name: string
+          role?: string | null
+          type: string
+          user_id?: string | null
+        }
         Update: {
-          category?: string;
-          created_at?: string;
-          id?: string;
-          name?: string;
-          type?: string;
-        };
-        Relationships: [];
-      };
+          category?: string
+          created_at?: string
+          id?: string
+          name?: string
+          role?: string | null
+          type?: string
+          user_id?: string | null
+        }
+        Relationships: []
+      }
       template: {
         Row: {
-          created_at: string;
-          exercise_id: string;
-          id: string;
-          user_id: string;
-        };
+          created_at: string
+          exercise_id: string
+          id: string
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          exercise_id?: string;
-          id?: string;
-          user_id: string;
-        };
+          created_at?: string
+          exercise_id?: string
+          id?: string
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          exercise_id?: string;
-          id?: string;
-          user_id?: string;
-        };
+          created_at?: string
+          exercise_id?: string
+          id?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "public_template_exercise_id_fkey";
-            columns: ["exercise_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
+            foreignKeyName: "public_template_exercise_id_fkey"
+            columns: ["exercise_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       users: {
         Row: {
-          created_at: string;
-          email: string;
-          id: string;
-          template_id: string;
-        };
+          created_at: string
+          email: string
+          id: string
+          template_id: string
+        }
         Insert: {
-          created_at?: string;
-          email: string;
-          id?: string;
-          template_id?: string;
-        };
+          created_at?: string
+          email: string
+          id?: string
+          template_id?: string
+        }
         Update: {
-          created_at?: string;
-          email?: string;
-          id?: string;
-          template_id?: string;
-        };
+          created_at?: string
+          email?: string
+          id?: string
+          template_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "public_users_template_id_fkey";
-            columns: ["template_id"];
-            isOneToOne: false;
-            referencedRelation: "template";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
-    };
+            foreignKeyName: "public_users_template_id_fkey"
+            columns: ["template_id"]
+            isOneToOne: false
+            referencedRelation: "template"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       requesting_user_id: {
-        Args: Record<PropertyKey, never>;
-        Returns: string;
-      };
-    };
+        Args: Record<PropertyKey, never>
+        Returns: string
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
 
 export type Tables<
   PublicTableNameOrOptions extends
     | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
-    | {schema: keyof Database},
-  TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
-> = PublicTableNameOrOptions extends {schema: keyof Database}
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-        Database["public"]["Views"])
-    ? (Database["public"]["Tables"] &
-        Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R;
-      }
-      ? R
-      : never
-    : never;
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends keyof Database["public"]["Tables"] | {schema: keyof Database},
-  TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = PublicTableNameOrOptions extends {schema: keyof Database}
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
-      }
-      ? I
-      : never
-    : never;
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends keyof Database["public"]["Tables"] | {schema: keyof Database},
-  TableName extends PublicTableNameOrOptions extends {schema: keyof Database}
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
-> = PublicTableNameOrOptions extends {schema: keyof Database}
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
-      }
-      ? U
-      : never
-    : never;
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
 
 export type Enums<
-  PublicEnumNameOrOptions extends keyof Database["public"]["Enums"] | {schema: keyof Database},
-  EnumName extends PublicEnumNameOrOptions extends {schema: keyof Database}
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
-> = PublicEnumNameOrOptions extends {schema: keyof Database}
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-    ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-    : never;
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
