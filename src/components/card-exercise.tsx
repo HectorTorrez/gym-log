de102baa1eb6cea63ec8c7afa8c45dc4bf45e83f
Hector <torrezhectorb@gmail.com>
2024-02-25 14:13:24 -0600

    feat: add delete button on exercises

--- src/components/card-exercise.tsx
+++ src/components/card-exercise.tsx
@@ -2,17 +2,60 @@
 
 import type {Exercise} from "@/types/exercise";
 
+import {Trash2} from "lucide-react";
+import {useAuth} from "@clerk/nextjs";
+import {useRouter} from "next/navigation";
+
+import {supabaseClient} from "@/db/api/server";
+
 import {Card, CardDescription, CardTitle} from "./ui/card";
+import {AlertDelete} from "./alert-delete";
+import {toast} from "./ui/use-toast";
 
 export default function CardExercise({exercise}: {exercise: Exercise[]}) {
+  const {userId, getToken} = useAuth();
+  const router = useRouter();
+
+  const handleDeleteExercise = async (id: string) => {
+    const supabaseAccessToken = await getToken({template: "gym-log"});
+
+    if (supabaseAccessToken === null) {
+      toast({
+        description: "Error delete exercise. Please log.",
+      });
+
+      return;
+    }
+
+    const supabase = await supabaseClient(supabaseAccessToken);
+    const {error} = await supabase.from("exercise_list").delete().eq("id", id);
+
+    if (error === null) {
+      router.refresh();
+    }
+  };
+
   return (
     <>
       {exercise.map((item) => {
         return (
           <Card key={item.id} className="flex flex-col gap-3 px-4 py-4">
-            <CardTitle>
-              {item.name} <span>({item.type})</span>
-            </CardTitle>
+            <section className="flex items-center justify-between text-wrap">
+              <CardTitle className="h-auto max-w-[70%] text-wrap">
+                {item.name} <span>({item.type})</span>
+              </CardTitle>
+              {userId === item.user_id && (
+                <AlertDelete
+                  textAction="Continue"
+                  textButton={<Trash2 color="red" size={20} />}
+                  textCancel="Cancel"
+                  textDescription="This action cannot be undone. This will permanently delete the exercise."
+                  textTitle="Are you absolutely sure?"
+                  variant="ghost"
+                  onClick={() => void handleDeleteExercise(item.id)}
+                />
+              )}
+            </section>
             <CardDescription>{item.category}</CardDescription>
           </Card>
         );
