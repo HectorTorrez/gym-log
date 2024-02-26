37f02fa13ec4b635fc13bb90f6b5eade32225a95
Hector <torrezhectorb@gmail.com>
2024-02-26 16:17:23 -0600

    fix: shadcn select don't work with an empty value and it was necessary to change it

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -1,29 +1,46 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import {usePathname, useSearchParams} from "next/navigation";
+import {useRouter} from "next/navigation";
 
 import {Input} from "./ui/input";
 
 export default function SelectCategory() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  function handleFilter(key: string, value: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+
+    router.replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <section className="flex w-[50%] flex-col gap-10 md:flex-row">
-      <Input className="w-[180px] max-w-[500px] md:w-[100%]" placeholder="Search" />
-      <Select>
-        <SelectTrigger className="w-[180px] md:w-[100%]">
-          <SelectValue placeholder="Filter by Category" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Chest</SelectItem>
-          <SelectItem value="dark">Legs</SelectItem>
-          <SelectItem value="system">Arms</SelectItem>
-          <SelectItem value="system">Shoulder</SelectItem>
-          <SelectItem value="system">Back</SelectItem>
-        </SelectContent>
-      </Select>
+      <Input
+        className="w-[180px] max-w-[500px] md:w-[100%]"
+        defaultValue={searchParams.get("name") ?? ""}
+        maxLength={20}
+        placeholder="Search"
+        onChange={(e) => handleFilter("name", e.target.value)}
+      />
+      <select
+        className="flex h-10 w-[180px] items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
+        defaultValue={searchParams.get("category") ?? ""}
+        onChange={(e) => handleFilter("category", e.target.value)}
+      >
+        <option value="">Todas las monedas</option>
+        <option value="chest">Chest</option>
+        <option value="legs">Legs</option>
+        <option value="arms">Arms</option>
+        <option value="shoulders">Shoulders</option>
+        <option value="back">Back</option>
+      </select>
     </section>
   );
 }
