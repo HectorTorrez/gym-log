01d93395ae0da3e931bd10aba29ba948cec16bcd
Hector Torrez <torrezhectorb@gmail.com>
2024-05-18 11:40:04 -0600

    fix/ ignoreRoute

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import {authMiddleware} from "@clerk/nextjs";
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default authMiddleware({
   // publicRoutes: ["/"],
-  ignoredRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/app/api/webhooks(.*)"],
 });
 
 export const config = {
